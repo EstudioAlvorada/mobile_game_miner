0f1b71775d5f85874bddc993d485f70d24658fc3
Marcs Primo <marcus.primov@gmail.com>
2021-06-27 11:40:01 -0300

    Mudanças no script do banco de dados

--- Assets/Scripts/DataBase.cs
+++ Assets/Scripts/DataBase.cs
@@ -16,122 +16,51 @@ namespace Assets.Scripts
     {
         IPlatform platform = new MarcelloDB.netfx.Platform();
 
-        public DataBase()
-        {
-            //string dataPath = $"{Application.dataPath}/Base/";
-
-            //var session = new MarcelloDB.Session(platform, dataPath);
-
-            //var baseFile = session["base.dat"];
-
-            //var construcoes = baseFile.Collection<Construcoes, string>("construcoes", cons => cons.tipo);
-
-            //foreach (var i in new string[] { "Casa", "Madeireira" })
-            //{
-            //    if (construcoes.Find(i) == null)
-            //    {
-            //        construcoes.Persist(new Construcoes() { tipo = i, numUpgrade = 1, pontosTotal = 0, ativo = true, ultimoTempo = 0f, velocidade = 1f });
-            //    }
-            //}
-
-            //var valores = baseFile.Collection<Valores, int>("valores", cons => cons.id);
-
-            //var teste = new List<ValorRecurso>();
-
-            //valores.Persist(new Valores() { id = 1, tipo = "Casa", nivel = 1, valorDinheiro = 120, ValorRecursos = teste });
-
-            //teste = new List<ValorRecurso>();
-            //teste.Add(new ValorRecurso("Madeireira", 70f));
-
-            //valores.Persist(new Valores() { id = 2, tipo = "Casa", nivel = 2, valorDinheiro = 200, ValorRecursos = teste });
-
-            //teste = new List<ValorRecurso>();
-            //teste.Add(new ValorRecurso("Madeireira", 150f));
-            //teste.Add(new ValorRecurso("Minerio", 90f));
-
-            //valores.Persist(new Valores() { id = 3, tipo = "Casa", nivel = 3, valorDinheiro = 280, ValorRecursos = teste });
-
-
-            //teste = new List<ValorRecurso>();
-            //teste.Add(new ValorRecurso("Madeireira", 240f));
-            //teste.Add(new ValorRecurso("Minerio", 210f));
-            //teste.Add(new ValorRecurso("Petroleo", 150f));
-
-            //valores.Persist(new Valores() { id = 4, tipo = "Casa", nivel = 4, valorDinheiro = 380, ValorRecursos = teste });
-
-            //teste = new List<ValorRecurso>();
-            //teste.Add(new ValorRecurso("Madeireira", 330f));
-            //teste.Add(new ValorRecurso("Minerio", 300f));
-            //teste.Add(new ValorRecurso("Minerio", 240f));
-
-            //valores.Persist(new Valores() { id = 5, tipo = "Casa", nivel = 5, valorDinheiro = 550, ValorRecursos = teste });
-
-            //foreach (var i in new string[] { "Casa", "Madeireira" })
-            //{
-            //    if (construcoes.Find(i) == null)
-            //    {
-            //    }
-
-            //    var teste = construcoes.Find(i);
-
-            //    Debug.Log($"{teste.tipo}");
-            //}
-
-            //session.Dispose();
-
-        }
+        public string dataPath = $"{Application.dataPath}/Base/";
 
         public void AutoSalvarPontuacoes()
         {
-            string dataPath = $"{Application.dataPath}/Base/";
-
-            var session = new MarcelloDB.Session(platform, dataPath);
-
-            var construcoesFile = session["base.dat"];
-
-            var construcoes = construcoesFile.Collection<Construcoes, string>("construcoes", cons => cons.tipo);
-
-            foreach (var i in new string[] { "Casa", "Madeireira", "Mineradora" })
+            using (var session = new MarcelloDB.Session(platform, dataPath))
             {
-                var tipo = GameManager.Instance.construcoes.FirstOrDefault(p => p.tipo == i);
+                var construcoesFile = session["base.dat"];
 
-                construcoes.Persist(new Construcoes() { tipo = i, numUpgrade = tipo.numUpgrade, pontosTotal = tipo.pontosTotal, ativo = tipo.ativo, ultimoTempo = 0f, velocidade = 1f });
-                
+                var construcoes = construcoesFile.Collection<Construcoes, string>("construcoes", cons => cons.tipo);
+
+                foreach (var i in new string[] { "Casa", "Madeireira", "Mineradora" })
+                {
+                    var tipo = GameManager.Instance.construcoes.FirstOrDefault(p => p.tipo == i);
+
+                    construcoes.Persist(new Construcoes() { tipo = i, numUpgrade = tipo.numUpgrade, pontosTotal = tipo.pontosTotal, ativo = tipo.ativo, ultimoTempo = 0f, velocidade = 1f });
+
+                }
             }
 
-            session.Dispose();
         }
 
         public void SaveConfig()
         {
-            string dataPath = $"{Application.dataPath}/Base/";
+            using (var session = new MarcelloDB.Session(platform, dataPath))
+            {
+                var baseFile = session["base.dat"];
 
-            var session = new MarcelloDB.Session(platform, dataPath);
+                var config = baseFile.Collection<Config, int>("config", cons => cons.id);
 
-            var baseFile = session["base.dat"];
+                config.Persist(new Config() { id = GameManager.Instance.config.id, aberto = GameManager.Instance.config.aberto, ultimoGame = DateTime.Now });
+            }
 
-            var config = baseFile.Collection<Config, int>("config", cons => cons.id);
-
-            config.Persist(new Config() {id = GameManager.Instance.config.id, aberto = GameManager.Instance.config.aberto, ultimoGame = DateTime.Now });
-
-            
-
-            session.Dispose();
         }
 
         public List<Construcoes> GetAllConstrucoes()
         {
-            string dataPath = $"{Application.dataPath}/Base/";
+            List<Construcoes> tudo = new List<Construcoes>();
+            using (var session = new MarcelloDB.Session(platform, dataPath))
+            {
+                var construcoesFile = session["base.dat"];
 
-            var session = new MarcelloDB.Session(platform, dataPath);
+                var construcoes = construcoesFile.Collection<Construcoes, string>("construcoes", cons => cons.tipo);
 
-            var construcoesFile = session["base.dat"];
-
-            var construcoes = construcoesFile.Collection<Construcoes, string>("construcoes", cons => cons.tipo);
-
-            var tudo = construcoes.All.ToList();
-
-            session.Dispose();
+                tudo = construcoes.All.ToList();
+            }
 
             return tudo;
         }
@@ -139,70 +68,65 @@ namespace Assets.Scripts
 
         public List<Valores> GetAllValores()
         {
-            string dataPath = $"{Application.dataPath}/Base/";
+            List<Valores> tudo = new List<Valores>();
 
-            var session = new MarcelloDB.Session(platform, dataPath);
+            using (var session = new MarcelloDB.Session(platform, dataPath))
+            {
+                var construcoesFile = session["base.dat"];
 
-            var construcoesFile = session["base.dat"];
+                var valores = construcoesFile.Collection<Valores, int>("valores", cons => cons.id);
 
-            var valores = construcoesFile.Collection<Valores, int>("valores", cons => cons.id);
-
-            var tudo = valores.All.ToList();
-
-            session.Dispose();
+                tudo = valores.All.ToList();
+            }
 
             return tudo;
         }
 
         public Config GetConfig()
         {
-            string dataPath = $"{Application.dataPath}/Base/";
+            var tudo = new Config();
 
-            var session = new MarcelloDB.Session(platform, dataPath);
+            using (var session = new MarcelloDB.Session(platform, dataPath))
+            {
+                var construcoesFile = session["base.dat"];
 
-            var construcoesFile = session["base.dat"];
+                var config = construcoesFile.Collection<Config, int>("config", cons => cons.id);
 
-            var config = construcoesFile.Collection<Config, int>("config", cons => cons.id);
+                config.All.ToList().ForEach(p => Debug.Log(p.aberto));
 
-            config.All.ToList().ForEach(p => Debug.Log(p.aberto));
-
-            var tudo = config.All.First();
-
-            session.Dispose();
+                tudo = config.All.First();
+            }
 
             return tudo;
         }
 
         public void CriarBanco()
         {
-            string dataPath = $"{Application.dataPath}/Base/";
-
             if(!File.Exists(dataPath + "base.dat"))
             {
-                var session = new MarcelloDB.Session(platform, dataPath);
-
-                var baseFile = session["base.dat"];
-
-                var construcoes = baseFile.Collection<Construcoes, string>("construcoes", cons => cons.tipo);
-
-                foreach (var i in new string[] { "Casa", "Madeireira", "Mineradora" })
+                using (var session = new MarcelloDB.Session(platform, dataPath))
                 {
-                    if (construcoes.Find(i) == null)
+                    var baseFile = session["base.dat"];
+
+                    var construcoes = baseFile.Collection<Construcoes, string>("construcoes", cons => cons.tipo);
+
+                    foreach (var i in new string[] { "Casa", "Madeireira", "Mineradora" })
                     {
-                        construcoes.Persist(new Construcoes() { tipo = i, numUpgrade = 1, pontosTotal = 0, pontosAcumulados = 0, ativo = i == "Mineradora" ? false : true, ultimoTempo = 0f, velocidade = 1f });
+                        if (construcoes.Find(i) == null)
+                        {
+                            construcoes.Persist(new Construcoes() { tipo = i, numUpgrade = 1, pontosTotal = 0, pontosAcumulados = 0, ativo = i == "Mineradora" ? false : true, ultimoTempo = 0f, velocidade = 1f });
+                        }
                     }
+
+                    var valores = baseFile.Collection<Valores, int>("valores", cons => cons.id);
+
+                    ValoresCasa(valores);
+                    ValoresMadeireira(valores);
+
+                    var config = baseFile.Collection<Config, int>("config", cons => cons.id);
+
+                    config.Persist(new Config() { id = 1, aberto = false, ultimoGame = DateTime.Now });
                 }
-
-                var valores = baseFile.Collection<Valores, int>("valores", cons => cons.id);
-
-                ValoresCasa(valores);
-                ValoresMadeireira(valores);
-
-                var config = baseFile.Collection<Config, int>("config", cons => cons.id);
-
-                config.Persist(new Config() { id = 1, aberto = false, ultimoGame = DateTime.Now });
-
-                session.Dispose();
             }
         }
 
